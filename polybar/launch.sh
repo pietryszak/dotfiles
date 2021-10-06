67a902d5df8c4b590f8ad0ca2adc570d8c5d7037
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2021-10-06 21:02:29 +0200

    Reload of polybar if needed

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,11 +2,12 @@
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
 
-# Launch polybar
-echo "---" | tee -a /tmp/polybar1.log
-polybar polybar 2>&1 | tee -a /tmp/polybar1.log & disown
+if type "xrandr"; then
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload mainbar &
+done
 
-echo "Bars launched..."
+else
+  polybar --reload mainbar &
+fi
