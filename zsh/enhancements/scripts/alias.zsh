de109df8b39ba0490b195469cd130ec1e5839203
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2021-10-25 22:46:07 +0200

    Create alias.zsh

---
+++ zsh/enhancements/scripts/alias.zsh
@@ -0,0 +1,60 @@
+# Zshrc
+alias zshrc='nvim ~/.zshrc'
+alias rld='source ~/.zshrc'
+
+# Alias
+alias als="bat ~/.config/oh-my-zsh/custom/enhancements/alias.zsh"
+
+# Neovim
+alias v="nvim"
+alias vi="nvim"
+alias neovim="nvim"
+alias vim="nvim"
+alias sv="sudo nvim"
+
+#Top
+alias top="htop"
+alias btop="bpytop"
+
+# Neofetch
+alias neo="neofetch"
+
+# Update
+alias update="sudo dnf update -y"
+
+# Install
+alias install="sudo dnf install -y"
+
+# Mkdir
+alias mk="mkdir"
+alias mkp="mkdir -p"
+
+# Rmdir
+alias rd="rm -ir"
+#
+# Sudo
+alias root="sudo su"
+alias s='sudo'
+
+# Exit
+alias exi="exit"
+
+# Ls
+alias ls="ls -alh"
+
+# BAT
+alias cat="bat"
+
+# Git
+alias gc="git clone"
+
+# Clear
+alias c="clear"
+
+# Command history
+alias h="history"
+
+# Cd 
+alias .='cd'
+alias ..='cd ..'
+alias ...='cd ../..'
