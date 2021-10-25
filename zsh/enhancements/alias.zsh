c8750aa49ff1291806f627906599b57ac9749f52
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2021-10-25 21:34:34 +0200

    Create alias.zsh

---
+++ zsh/enhancements/alias.zsh
@@ -0,0 +1,13 @@
+# Zshrc
+alias zshrc='nvim ~/.zshrc'
+alias rld='source ~/.zshrc'
+
+# Neovim
+alias v="nvim"
+alias vi="nvim"
+alias neovim="nvim"
+alias vim="nvim"
+
+# Top
+alias top="htop"
+alias btop="bpytop"
