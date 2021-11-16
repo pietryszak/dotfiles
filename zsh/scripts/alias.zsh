5bab26413b6338935c42aed0b3388542314cda96
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2021-11-17 00:14:19 +0100

    pku update

--- zsh/scripts/alias.zsh
+++ zsh/scripts/alias.zsh
@@ -58,7 +58,7 @@ alias ..='cd ..'
 alias ...='cd ../..'
 
 # Logout
-alias pku="pkill -u $USER"
+alias pku="exec swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' -B 'Yes, exit sway' 'swaymsg exit'"
 
 # Exit
 alias ext="exit"
