09702225574f91da7d7d2aff4760f2974c75005d
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2021-11-23 18:56:05 +0100

    Create vertical.sh

---
+++ screenlayout/vertical.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-1 --off --output HDMI-1 --off --output DP-1 --off --output HDMI-2 --mode 2560x1440 --pos 2560x0 --rotate right --output DP-1-8 --primary --mode 2560x1440 --pos 0x761 --rotate normal --output DP-1-0 --off
