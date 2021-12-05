e880cc4d00e20a920c0fe0b642815f2a5ffdb33c
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2021-12-05 12:51:29 +0100

    Create horizontal.sh

---
+++ screenlayout/horizontal.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+xrandr --output HDMI-2 --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-1-8 --primary --mode 2560x1440 --pos 0x0 --rotate normal
+
+dir="$HOME/.config/polybar"
+themes=(`ls --hide="launch.sh" $dir`)
+
+launch_bar() {
+	killall -q polybar
+	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+	if [[ "$style" == "cuts" ]]; then
+		polybar -q first -c "$dir/$style/config.ini" &
+		polybar -q second -c "$dir/$style/config.ini" &
+
+	else
+		#👇 👉 launching multiple monitors --> make sure to add monitor = ${env:MONITOR:} in the config
+		if type "xrandr"; then
+		  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		    MONITOR=$m polybar -q main -c "$dir/$style/config.ini" &
+		  done
+		else
+		  polybar -q main -c "$dir/$style/config.ini" &
+		fi
+	fi
+}
+
+
+if [[ "$1" == "--cuts" ]]; then
+	style="cuts"
+	launch_bar
+
+else
+	cat <<- EOF
+	Usage : launch.sh --theme
+		
+	Available Themes :
+	--cuts
+	EOF
+fi
