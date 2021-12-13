e592fa95bff11ce7b1146090282cfea9ef410374
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2021-12-13 21:58:48 +0100

    Update all dotfiles in system

--- update/update.sh
+++ update/update.sh
@@ -64,3 +64,115 @@ cp ~/.gc/polybar-spotify/spotify_status.py ~/.config/polybar/scripts/
 sed -i -e '/play_pause/s/25B6/F909/' ~/.config/polybar/scripts/spotify_status.py 
 sed -i -e '/play_pause/s/23F8/F8E3/' ~/.config/polybar/scripts/spotify_status.py 
 cd
+
+
+#######################################################################
+# My dotfiles update
+#######################################################################
+
+rm -rf ~/.gc/dotfiles
+
+# My dotfiles
+cd ~/.gc
+git clone https://github.com/pietryszak/dotfiles.git
+cd
+
+# Copy bat  config to proper folder
+\cp -r ~/.gc/dotfiles/bat ~/.config
+
+# Copy Code config to proper folder
+\cp -r ~/.gc/dotfiles/Code/settings.json ~/.config/Code/User/
+
+# Copy htop config to proper folder 
+\cp -r ~/.gc/dotfiles/htop ~/.config
+
+# Copy neofetch config to proper folder
+\cp -r ~/.gc/dotfiles/neofetch ~/.config
+
+# Copy nvim config to proper folder
+\cp -r ~/.gc/dotfiles/nvim ~/.config
+
+# Copy shortcuts list to proper folder
+\cp -r ~/.gc/dotfiles/shortcuts ~/.config
+
+# Copy spotifyd config to proper folder
+\cp -r ~/.gc/dotfiles/spotifyd ~/.config
+
+# Copy spotify-tui config to proper folder
+\cp -r ~/.gc/dotfiles/spotify-tui ~/.config
+
+# Copy VirtualBox config to proper folder 
+\cp -r ~/.gc/dotfiles/VirtualBox ~/.config
+chmod +x ~/.config/VirtualBox/update.sh
+
+# Copy VmWare config to proper folder
+\cp -r ~/.gc/dotfiles/vmware/preferences ~/.vmware
+
+# Copy Caprine config to proper folder
+\cp -r ~/.gc/dotfiles/Caprine ~/.config
+
+# Copy zsh sripts to proper folder
+\cp -r ~/.gc/dotfiles/zsh/scripts/* ~/.oh-my-zsh/custom
+
+# Copy zshrc config to proper folder
+\cp -r ~/.gc/dotfiles/zsh/.zshrc ~/
+
+# Copy powerlevel10k config to proper folder
+\cp -r ~/.gc/dotfiles/zsh/.p10k.zsh ~/
+
+# Copy terminator config to proper folder
+\cp -r ~/.gc/dotfiles/terminator/ ~/.config
+
+# Copy kitty config to proper folder
+\cp -r ~/.gc/dotfiles/kitty/ ~/.config
+
+# Copy TeamViewer config to proper folder
+\cp -r ~/.gc/dotfiles/teamviewer/ ~/.config
+
+# Copy Redshitf config to proper folder
+\cp -r ~/.gc/dotfiles/redshift/ ~/.config
+
+# Copy bash_aliases to user folder
+\cp -r ~/.gc/dotfiles/bashrc/.bash_aliases ~/ 
+
+# Copy bash_aliases to sudo/root folder
+sudo \cp -r ~/.gc/dotfiles/bashrc/.bash_aliases /root  
+
+# Copy qt5ct config to to proper folder
+\cp -r ~/.gc/dotfiles/qt5ct ~/.config
+
+# Copy gtk config to to proper folder
+cp ~/.gc/dotfiles/gtk/.gtkrc-2.0 ~
+cp ~/.gc/dotfiles/gtk/settings.ini ~/.config/gtk-3.0/
+
+# Copy gedit config to to proper folder
+sudo \cp -r ~/.gc/dotfiles/gedit/* /usr/share/gtksourceview-4/styles
+gsettings set org.gnome.gedit.preferences.editor scheme 'gruvbox-dark' 
+
+# Copy arandr config to to proper folder
+\cp -r ~/.gc/dotfiles/screenlayout/* ~/.screenlayout
+chmod +x ~/.screenlayout/*
+
+# Copy shortcuts list to proper folder
+\cp -r ~/.gc/dotfiles/shortcuts ~/.config
+
+# Copy i3 config to to proper folder
+\cp -r ~/.gc/dotfiles/i3 ~/.config
+rm ~/.config/i3/scripts/vmware-workspaces
+
+# Copy polybar config to to proper folder
+\cp -r ~/.gc/dotfiles/polybar ~/.config
+chmod +x ~/.config/polybar/cuts/scripts/launcher.sh
+chmod +x ~/.config/polybar/cuts/scripts/powermenu.sh
+chmod +x ~/.config/polybar/scripts/*
+cp ~/.gc/polybar-spotify/spotify_status.py ~/.config/polybar/scripts/
+sed -i -e '/play_pause/s/25B6/F909/' ~/.config/polybar/scripts/spotify_status.py 
+sed -i -e '/play_pause/s/23F8/F8E3/' ~/.config/polybar/scripts/spotify_status.py 
+
+# Copy volumeicon config to to proper folder
+rm -rf ~/.config/volumeicon
+\cp -r ~/.gc/dotfiles/volumeicon/* ~/.config
+
+# Copy update script to to proper folder
+\cp -r ~/.gc/dotfiles/update/* ~/.scripts
+chmod +x ~/.scripts/update.sh
