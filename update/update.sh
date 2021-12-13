0b116b5483a1cd8ecd9b01652a3ee290f3606ee8
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2021-12-13 21:33:49 +0100

    Add powerlevel10k update

--- update/update.sh
+++ update/update.sh
@@ -41,3 +41,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 # Update zsh syntax autosuggestions
 rm -rf ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Update powerlevel10k
+rm -rf ~/.oh-my-zsh/custom/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
