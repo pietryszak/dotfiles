411045a56c75184560892a82c197be2f0cd89a9a
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2021-12-14 02:52:30 +0100

    Update flatpaks

--- update/update.sh
+++ update/update.sh
@@ -75,6 +75,10 @@ sudo python setup.py -q install
 sudo glib-compile-schemas /usr/share/glib-2.0/schemas > /dev/null 2>&1
 sudo rm  /usr/share/applications/caffeine-preferences.desktop 
 
+# Update flatpaks
+echo "${red}${bold}UPDATE FLATPAKS${reset}"
+flatpak update 
+
 #######################################################################
 # My dotfiles update
 #######################################################################
