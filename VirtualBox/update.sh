598ecf4bcff6127c9693bc63cefdcee2e8c35f04
Paweł Pietryszak <pietryszak@outlook.com>
2022-10-15 20:11:51 +0200

    Bat, btop config

--- VirtualBox/update.sh
+++
@@ -1,4 +0,0 @@
-cd ~/.gc/VirtualBox &&
-LatestVirtualBoxVersion=$(wget -qO - https://download.virtualbox.org/virtualbox/LATEST-STABLE.TXT) && wget "https://download.virtualbox.org/virtualbox/${LatestVirtualBoxVersion}/Oracle_VM_VirtualBox_Extension_Pack-${LatestVirtualBoxVersion}.vbox-extpack" &&
-yes | sudo VBoxManage extpack install --replace Oracle_VM_VirtualBox_Extension_Pack-${LatestVirtualBoxVersion}.vbox-extpack &&
-cd
