1312f0f6ea103eedf034c6f14585182438a374fb
Paweł Pietryszak <pietryszak@outlook.com>
2022-10-16 17:53:26 +0200

    Screen layout for virtualbox
    Screen layout for virtualbox

---
+++ screenlayout/virtualbox.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+xrandr --output Virtual1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output Virtual2 --off --output Virtual3 --off --output Virtual4 --off --output Virtual5 --off --output Virtual6 --off --output Virtual7 --off --output Virtual8 --off   
+    
+    killall -q polybar
+	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+		polybar -q one-monitor -c "~/.config/polybar/cuts/config.ini" &
+	
+	killall -q picom && sleep 1
+	picom --config ~/.config/picom.conf
