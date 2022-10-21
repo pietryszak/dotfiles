98bccd4b77f0ccdfbbce0e529049ea9c6bc38b61
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2022-10-22 00:50:14 +0200

    Change height

--- polybar/scripts/popup-calendar.sh
+++ polybar/scripts/popup-calendar.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-BAR_HEIGHT=32  # polybar height
+BAR_HEIGHT=64  # polybar height
 BORDER_SIZE=1  # border size from your wm settings
 YAD_WIDTH=444  # 222 is minimum possible value
 YAD_HEIGHT=193 # 193 is minimum possible value
