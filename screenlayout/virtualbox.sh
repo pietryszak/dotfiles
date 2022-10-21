08790702a443c27e1ecd1a510dcbe5d39dd97c88
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2022-10-22 01:11:16 +0200

    Update virtualbox.sh

--- screenlayout/virtualbox.sh
+++ screenlayout/virtualbox.sh
@@ -4,7 +4,7 @@ xrandr --output Virtual1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --
     killall -q polybar
 	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-		polybar -q one-monitor -c "~/.config/polybar/cuts/config.ini" &
+		polybar -q virtualbox -c "~/.config/polybar/cuts/config.ini" &
 	
 	killall -q picom && sleep 1
 	picom --config ~/.config/picom.conf
