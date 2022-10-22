850607fdbaa0492d79ab0c0c2fc6c4ef604f6c0f
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2022-10-22 02:31:34 +0200

    Polybar settings

--- polybar/scripts/tinybar-virtualbox.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-killall -q dropbox
-	dropbox start &
-
-polybar -q tray -c "$HOME/.config/polybar/cuts/config.ini" &
--- screenlayout/virtualbox.sh
+++ screenlayout/virtualbox.sh
@@ -4,7 +4,7 @@ xrandr --output Virtual1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --
     killall -q polybar
 	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-		polybar -q one-monitor -c "~/.config/polybar/cuts/config-virtualbox.ini" &
+		polybar -q one-monitor -c "~/.config/polybar/cuts/config.ini" &
 	
 	killall -q picom && sleep 1
 	picom --config ~/.config/picom.conf
