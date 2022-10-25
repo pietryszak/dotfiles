ebc9b8a78c9343ddbe59cf32a3de9cede32c0fb5
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2022-10-25 16:43:14 +0200

    Data format change

--- polybar/scripts/popup-calendar.sh
+++ polybar/scripts/popup-calendar.sh
@@ -4,7 +4,7 @@ BAR_HEIGHT=64  # polybar height
 BORDER_SIZE=1  # border size from your wm settings
 YAD_WIDTH=480  # 222 is minimum possible value
 YAD_HEIGHT=193 # 193 is minimum possible value
-DATE="$(date +"  %a, %d %b %Y   %H:%M")"
+DATE="$(date +" %a %d.%b.%Y  %H:%M")"
 
 case "$1" in
 --popup)
