e29f0414d89944207bff87dbbc88a4e25561a0b4
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2022-11-02 02:31:51 +0100

    One monitor settings

---
+++ polybar/scripts/killbar-one-monitor.sh
@@ -0,0 +1,9 @@
+# kill script is updated to Version 2.0 with the help of pietryszak(github) idea
+# script by phon31x & pietryszak
+#! /bin/bash
+
+killall -q dropbox
+	dropbox start &
+
+u=$(xprop -name "polybar-one-monitor" _NET_WM_PID | grep -o '[[:digit:]]*')
+kill $u
